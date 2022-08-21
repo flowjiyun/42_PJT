ab57199f78cf8ff8a363cd9a303c1d594f14aeb2
flowjiyun <gyp2795@gmail.com>
2022-08-21 20:16:26 +0900

    featured : leak check

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: jiyun <jiyun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 18:56:19 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/08/21 17:08:55 by jiyun            ###   ########.fr       */
+/*   Updated: 2022/08/21 20:14:33 by jiyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,13 @@ int	main(void)
 	{
 		str = get_next_line(fd);
 		printf("%s", str);
+		free(str);
 		if (!str)
+		{
 			break ;
+		}
 		i++;
 	}
+	system("leaks a.out > leaks_result_temp; cat leaks_result_temp | grep leaked && rm -rf leaks_result_temp");
 	return (0);
 }
