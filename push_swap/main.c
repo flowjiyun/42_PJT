720a4fcf3b4c36da1262300cb6277b4a16088ebf
Jiyun Park <jiyunpar@c6r2s5.42seoul.kr>
2022-08-25 14:02:46 +0900

    check git

--- push_swap/main.c
+++ push_swap/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jiyunpar <jiyunpar@student.42seou.kr>      +#+  +:+       +#+        */
+/*   By: jiyunpar <jiyunpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 17:24:25 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/08/23 18:46:50 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/08/25 14:01:49 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // check each argv with isdigit to figure out if input is integer
 int	is_valid_input(int argc, char **argv)
 {
-	int	i;	
+	int	i;
 
 	if (argc == 0)
 		return (1);
@@ -26,5 +26,10 @@ int	main(int argc, char **argv)
 {
 	if (is_valid_input(argc, argv))
 		write(2, "Error\n", 6);
+	return (0);
+}
+
+int	call(int a)
+{
 	return (0);
 }
