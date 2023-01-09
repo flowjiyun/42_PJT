72de6c13a02deba274a766d2b83908aec8de3487
flowjiyun <gyp2795@gmail.com>
2023-01-09 16:05:57 +0900

    feat: add terminate_philo

--- philo/philo/src/wait_philo.c
+++ philo/philo/src/wait_philo.c
@@ -6,11 +6,25 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:34:49 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/01/06 16:17:24 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/01/09 14:36:31 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+bool	terminate_philo(t_philo **philo_arr, int num_of_philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_of_philo)
+	{
+		if (pthread_detach(*(philo_arr[i]->thread)) != 0)
+			return (false);
+		++i;
+	}
+	return (true);
+}
 
 bool	wait_philo(t_philo **philo_arr, int num_of_philo)
 {
