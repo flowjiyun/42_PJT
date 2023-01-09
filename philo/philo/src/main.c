8a05d5c7b6c26b6018c1560185cc7f63ac587cb2
flowjiyun <gyp2795@gmail.com>
2023-01-09 16:07:02 +0900

    feat: add logic to get simulation start time

--- philo/philo/src/main.c
+++ philo/philo/src/main.c
@@ -12,11 +12,30 @@
 
 #include "philo.h"
 
+bool	is_all_philo_created(t_shared_data *shared_data, int num_of_philo)
+{
+	int	i;
+
+	i = 0;
+	pthread_mutex_lock(&shared_data->created_flag_lock);
+	while (i < num_of_philo)
+	{
+		if (shared_data->is_philo_created[i] == false)
+		{
+			pthread_mutex_unlock(&shared_data->created_flag_lock);
+			return (false);
+		}
+		++i;
+	}
+	pthread_mutex_unlock(&shared_data->created_flag_lock);
+	return (true);
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo		**philo_arr;
-	t_input		*input;
-	t_fork		*fork;
+	t_philo			**philo_arr;
+	t_input			*input;
+	t_shared_data	*shared_data;
 
 	if (argc != 5 && argc != 6)
 	{
@@ -34,13 +53,13 @@ int	main(int argc, char **argv)
 		ft_putendl_fd("Error : set_input error", 2);
 		return (1);
 	}
-	fork = set_fork(input);
-	if (!fork)
+	shared_data = set_shared_data(input);
+	if (!shared_data)
 	{
 		ft_putendl_fd("Error : set_fork error", 2);
 		return (1);
 	}
-	philo_arr = set_philo(fork, input);
+	philo_arr = set_philo(shared_data, input);
 	if (!philo_arr)
 	{
 		ft_putendl_fd("Error : set_philo error", 2);
@@ -50,6 +69,30 @@ int	main(int argc, char **argv)
 	{
 		ft_putendl_fd("Error : create_philo error", 2);
 		return (1);
+	}
+	while (true)
+	{
+		if (is_all_philo_created(shared_data, input->num_of_philo) == true)
+		{
+			gettimeofday(&shared_data->simulation_start_time, NULL);
+			shared_data->start_flag = true;
+			printf("sec : %ld, microsec : %d\n", shared_data->simulation_start_time.tv_sec, shared_data->simulation_start_time.tv_usec);
+			// terminate_philo(philo_arr, input->num_of_philo);
+			// destroy_mutex(shared_data);
+			// return (0);
+			break ;
+		}
+		// if (argc == 6
+		// 	&& has_enough_meal(shared_data, input->num_of_philo) == true)
+		// {
+		// 	terminate_philo(philo_arr, input->num_of_philo);
+		// 	return (0);
+		// }
+		// if (is_philo_dead(shared_data, input->num_of_philo) == true)
+		// {
+		// 	terminate_philo(philo_arr, input->num_of_philo);
+		// 	return (0);
+		// }
 	}
 	if (wait_philo(philo_arr, input->num_of_philo) == false)
 	{
--- philo/philo/src/routine.c
+++ philo/philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:33:39 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/01/06 16:17:34 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/01/09 16:01:38 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 void	*routine(void *arg)
 {
-	t_philo	*philo;
+	t_philo			*philo;
 
 	philo = (t_philo *)arg;
-	printf("thread_id : %d\n", philo->thread_id);
+	// 쓰레드 생성 표시 남기기
+	pthread_mutex_lock(&philo->shared_data->created_flag_lock);
+	philo->shared_data->is_philo_created[philo->thread_id] = true;
+	pthread_mutex_unlock(&philo->shared_data->created_flag_lock);
+	// 다른쓰레드 모두 생성될 때 까지 대기
+	while (true)
+	{
+		if (philo->shared_data->start_flag == true)
+			break ;
+	}
+	printf("thread_id : %d, sec : %ld, microsec : %d\n", philo->thread_id, philo->shared_data->simulation_start_time.tv_sec, philo->shared_data->simulation_start_time.tv_usec);
+	// eating();
+	// putdown_fork();
+	// sleeping();
+	// thinking();
 	return (NULL);
 }
