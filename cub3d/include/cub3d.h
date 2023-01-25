afa602a4cfe3880c33ac8436028d2de9f8cf00d1
flowjiyun <gyp2795@gmail.com>
2023-01-25 16:24:47 +0900

    feat: add tutorial for floor

---
+++ cub3d/include/cub3d.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/21 16:17:08 by jiyunpar          #+#    #+#             */
+/*   Updated: 2023/01/21 16:20:26 by jiyunpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include "libft.h"
+# include "mlx.h"
+#endif
