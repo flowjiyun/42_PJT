0806834679c35ed56d64f4696c2dfb8e6a68016d
flowjiyun <gyp2795@gmail.com>
2023-02-16 17:41:59 +0900

    feat: md2 ex02 init

---
+++ CPP/module02/ex02/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 14:31:24 by jiyunpar          #+#    #+#             */
+/*   Updated: 2023/02/16 16:24:27 by jiyunpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	Fixed::getRawBit(void) const
+{
+	std::cout << "getRawBits member fucntion called" <<std::endl;
+	return (mFixedPointValue);
+}
+
+void Fixed::setRawBit(const int raw)
+{
+	mFixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)mFixedPointValue / (1 << 8));
+}
+
+int Fixed::toInt(void) const
+{
+	return ((int)mFixedPointValue / (1 << 8));
+}
+
+Fixed::Fixed()
+	: mFixedPointValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value)
+	: mFixedPointValue(value * (1 << mFractionalBit))
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value)
+	: mFixedPointValue(roundf(value * (1 << mFractionalBit)))
+{
+	std::cout << "Float constructor called" << std::endl;	
+}
+
+Fixed::~Fixed()
+{
+	std::cout <<"Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed&	Fixed::operator=(const Fixed& rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		mFixedPointValue = rhs.mFixedPointValue;
+	return (*this);
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& rhs)
+{
+	out << rhs.toFloat();
+	return (out);
+}
---
+++ CPP/module02/ex02/Fixed.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 14:31:32 by jiyunpar          #+#    #+#             */
+/*   Updated: 2023/02/16 17:35:31 by jiyunpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <cmath>
+class Fixed
+{
+	public:
+		// orthodox canonical form
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
+		~Fixed();
+		Fixed(const Fixed& other);
+		Fixed&	operator=(const Fixed& rhs);
+		// operator overload(comparison)
+		bool	operator>(const Fixed& rhs);
+		bool	operator<(const Fixed& rhs);
+		bool	operator>=(const Fixed& rhs);
+		bool	operator<=(const Fixed& rhs);
+		bool	operator==(const Fixed& rhs);
+		bool	operator!=(const Fixed& rhs);
+		// operator overload(arithmetic)
+		Fixed&	operator+(const Fixed& rhs);
+		Fixed&	operator-(const Fixed& rhs);
+		Fixed&	operator*(const Fixed& rhs);
+		Fixed&	operator/(const Fixed& rhs);
+		// operator overload(increment/decrement)
+		Fixed&	operator++(void);
+		Fixed&	operator++(int dummy);
+		Fixed&	operator--(void);
+		Fixed&	operator--(int dummy);
+
+		int		getRawBit(void) const;
+		void	setRawBit(const int raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		static Fixed& min(Fixed& a, Fixed& b);
+		static Fixed& min(const Fixed& a,  const Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		static Fixed& max(const Fixed& a,  const Fixed& b);
+
+	private:
+		int					mFixedPointValue;
+		static const int	mFractionalBit = 8;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& rhs);
+
+#endif
