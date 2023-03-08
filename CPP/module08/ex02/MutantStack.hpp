0336469ba71fc2fbd27d376faea20855c8f4ebd1
flowjiyun <gyp2795@gmail.com>
2023-03-08 16:36:12 +0900

    feat: md08 ex02

--- CPP/module08/ex02/MutantStack.hpp
+++ CPP/module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 17:23:16 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/07 16:56:11 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/03/08 16:28:34 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,52 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		typedef typename std::stack<T>::
-		std::deque<T>
+		typedef typename std::stack<T>::container_type::const_iterator iterator;
+
+		MutantStack();
+		MutantStack(const MutantStack<T>& other);
+		~MutantStack();
+		MutantStack<T>& operator=(const MutantStack<T>& rhs);
+
+		iterator begin(void) const;
+		iterator end(void) const;		
 };
+
+template <typename T>
+MutantStack<T>::MutantStack()
+	: std::stack<T>()
+{
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T>& other)
+	: std::stack<T>(other.c)
+{
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T>& rhs)
+{
+	if (this != &rhs)
+		this->c = rhs.c;
+	return (*this);
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin(void) const
+{
+	return (this->c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end(void) const
+{
+	return (this->c.end());
+}
+
 #endif
--- CPP/module08/ex02/main.cpp
+++ CPP/module08/ex02/main.cpp
@@ -6,7 +6,44 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 17:23:04 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/06 17:23:05 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/03/08 16:25:34 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "MutantStack.hpp"
+
+int main(void)
+{
+	MutantStack<int> a;
+
+	a.push(5);
+	a.push(10);
+
+	MutantStack<int> b(a);
+	std::cout << "a top is : " << a.top() << std::endl;
+	std::cout << "b top is : " << b.top() << std::endl;
+
+	a.pop();
+	std::cout << "a size is " << a.size() << std::endl;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		a.push(15 + i * 5);
+	}
+
+	MutantStack<int>::iterator it = a.begin();
+	MutantStack<int>::iterator ite = a.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	std::stack<int> s(a);
+	return(0);
+}
