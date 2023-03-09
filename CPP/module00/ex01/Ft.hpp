6361bf4216229021da1ba18ee577dfa519d2dde2
flowjiyun <gyp2795@gmail.com>
2023-03-09 12:01:03 +0900

    feat : make util getline to exit program when eof(ctrl + d)

---
+++ CPP/module00/ex01/Ft.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ft.hpp                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 11:10:27 by jiyunpar          #+#    #+#             */
+/*   Updated: 2023/03/09 11:23:06 by jiyunpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_HPP
+# define FT_HPP
+# include <iostream>
+# include <string>
+
+class Ft
+{
+	public:
+		static void getline(std::istream& in, std::string& val);
+};
+
+#endif
