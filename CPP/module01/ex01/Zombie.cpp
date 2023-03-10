aae6155c686df1f9709538ee67542bec2af7a1e5
flowjiyun <gyp2795@gmail.com>
2023-03-10 10:57:21 +0900

    feat : add constructor calling message

--- CPP/module01/ex01/Zombie.cpp
+++ CPP/module01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 12:44:40 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/02 15:16:14 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/03/10 10:55:02 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 Zombie::Zombie()
 	: mName("void")
 {	
+	std::cout << "Zombie defalt constructor called" << std::endl;
 }
 
 Zombie::Zombie(std::string name)
 	: mName(name)
 {
-
+	std::cout << "Zombie name constructor called" << std::endl;
 }
 
 Zombie::~Zombie()
