5b95a0b439640c5b487df00173a569283e2ba84b
flowjiyun <gyp2795@gmail.com>
2023-03-21 15:39:33 +0900

    fix : change variable name

--- CPP/module04/ex01/main.cpp
+++ CPP/module04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 12:49:55 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/21 15:15:43 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:30:05 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,24 @@ int main(void)
 {
 	{
 		std::cout << "===========Test1============" << std::endl;
-		Animal* animal[4];
+		Animal* cage[4];
 
 		// init animal pointer array
 		for (int i = 0; i < 4; ++i)
 		{
 			if (i < 2)
-				animal[i] = new Cat();
+				cage[i] = new Cat();
 			else
-				animal[i] = new Dog();
+				cage[i] = new Dog();
 		}
 
 		// make sound
 		for (int i = 0; i < 4; ++i)
-			animal[i]->makeSound();
+			cage[i]->makeSound();
 
 		// delete animal	
 		for (int i = 0; i < 4; ++i)
-			delete animal[i];
+			delete cage[i];
 	}
 		
 	std::cout << "===========Test2============" << std::endl;
