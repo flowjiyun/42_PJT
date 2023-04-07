be7235b0d7dd965a8e7ec0276c6d3f4babc2d287
flowjiyun <gyp2795@gmail.com>
2023-04-07 12:45:20 +0900

    fix : change typename

--- CPP/module08/ex00/easyfind.hpp
+++ CPP/module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 16:16:15 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/02 17:58:07 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/04/07 12:37:47 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 # define EASYFIND_HPP
 # include <algorithm>
 
-template <typename Container>
-typename Container::iterator easyfind(Container& container, int toFind)
+template <typename T>
+typename T::iterator easyfind(T& container, int toFind)
 {
-	return (find(container.begin(), container.end(), toFind));
+	return (std::find(container.begin(), container.end(), toFind));
 }
 
 #endif
