8fa32be635c3c951fd4d91844bd8755a56b2da8b
flowjiyun <gyp2795@gmail.com>
2023-04-09 23:52:53 +0900

    feat: microshell

--- exam/exam_04/microshell.h
+++ exam/exam_04/microshell.h
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 09:32:16 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/21 10:29:28 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/04/09 23:34:27 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h>
+# include <sys/wait.h>
 
 
 #endif 
