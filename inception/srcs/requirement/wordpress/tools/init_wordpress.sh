2fb77866de35c47d586832b2fda5caef67a636d9
flowjiyun <gyp2795@gmail.com>
2023-04-17 19:44:00 +0900

    fix: change nginx conf file & wordpress init script

--- inception/srcs/requirement/wordpress/tools/init_wordpress.sh
+++ inception/srcs/requirement/wordpress/tools/init_wordpress.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
-chown -R nginx:nginx /var/www/wordpress
-chmod -R 755 /var/www/wordpress
+chown -R nginx:nginx /var/www/html/wordpress
+chmod -R 755 /var/www/html/wordpress
 exec php-fpm8 -F
