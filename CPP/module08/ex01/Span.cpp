c87b5f2b375f4d3e99f9260abcbdd716cdab2cc9
flowjiyun <gyp2795@gmail.com>
2023-04-17 16:31:38 +0900

    feat : update Span

--- CPP/module08/ex01/Span.cpp
+++ CPP/module08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:38:26 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/23 17:12:30 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/04/17 16:30:13 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 const char* Span::MaxElement::what(void) const throw()
 {
 	return ("Can't add Number : Max Element");
+}
+
+const char* Span::NotEnoughElement::what(void) const throw()
+{
+	return ("Not Enough Elemnet to get Span");
 }
 
 Span::Span(unsigned int max)
@@ -47,7 +52,7 @@ Span& Span::operator=(const Span& rhs)
 	return (*this);
 }
 
-void Span::addNumber(unsigned int val)
+void Span::addNumber(int val)
 {
 	if (mSize >= mCapacity)
 		throw MaxElement();
@@ -58,9 +63,9 @@ void Span::addNumber(unsigned int val)
 	}
 }
 
-void Span::addNumber(std::vector<unsigned int>::const_iterator begin, std::vector<unsigned int>::const_iterator end)
+void Span::addNumber(std::vector<int>::const_iterator begin, std::vector<int>::const_iterator end)
 {
-	for (std::vector<unsigned int>::const_iterator it = begin; it != end; ++it)
+	for (std::vector<int>::const_iterator it = begin; it != end; ++it)
 	{
 		if (mSize >= mCapacity)
 			throw MaxElement();
@@ -71,20 +76,20 @@ void Span::addNumber(std::vector<unsigned int>::const_iterator begin, std::vecto
 
 void Span::prinfNumber(void) const
 {
-	for (std::vector<unsigned int>::const_iterator it = mVec.begin(); it != mVec.end(); ++it)
+	for (std::vector<int>::const_iterator it = mVec.begin(); it != mVec.end(); ++it)
 		std::cout << *it << std::endl;
 }
 
 unsigned int Span::shortestSpan(void) const
 {
 	unsigned int min;
-	std::vector<unsigned int> temp(mVec);
+	std::vector<int> temp(mVec);
 
 	min = UINT_MAX;
 	sort(temp.begin(), temp.end());
-	for (std::vector<unsigned int>::const_iterator it = temp.begin(); it != temp.end() - 1; ++it)
+	for (std::vector<int>::const_iterator it = temp.begin(); it != temp.end() - 1; ++it)
 	{
-		std::vector<unsigned int>::const_iterator itNext = it + 1;
+		std::vector<int>::const_iterator itNext = it + 1;
 		// exception check`
 		if (*itNext - *it < min)
 			min = *itNext - *it;
@@ -94,10 +99,8 @@ unsigned int Span::shortestSpan(void) const
 
 unsigned int Span::longestSpan(void) const
 {
-	std::vector<unsigned int> temp(mVec);
+	std::vector<int> temp(mVec);
 	
 	sort(temp.begin(), temp.end());
 	return (temp.back() - temp.front());
 }
-
-1 4 5 6 7
--- CPP/module08/ex01/Span.hpp
+++ CPP/module08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:38:19 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/03/06 17:41:32 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/04/07 12:59:18 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,24 @@ class Span
 			public:
 				virtual const char* what(void) const throw();
 		};
+		class NotEnoughElement : public std::exception
+		{
+			public:
+				virtual const char* what(void) const throw();
+		};
 		Span(unsigned int max);
 		Span(const Span& other);
 		~Span();
 		Span& operator=(const Span& rhs);
 
-		void addNumber(unsigned int val);
-		void addNumber(std::vector<unsigned int>::const_iterator begin, std::vector<unsigned int>::const_iterator end);
+		void addNumber(int val);
+		void addNumber(std::vector<int>::const_iterator begin, std::vector<int>::const_iterator end);
 		unsigned int shortestSpan(void) const;
 		unsigned int longestSpan(void) const;
 		void prinfNumber(void) const;
 
 	private:
-		std::vector<unsigned int> mVec;
+		std::vector<int> mVec;
 		unsigned int mSize;
 		unsigned int mCapacity;
 		Span();
