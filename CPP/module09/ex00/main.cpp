f29dbb8fbd1d1777056fd06f4303d4a4bb96e523
flowjiyun <gyp2795@gmail.com>
2023-08-29 13:34:50 +0900

    fix: delete unused line

--- CPP/module09/ex00/main.cpp
+++ CPP/module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 15:25:25 by jiyunpar          #+#    #+#             */
-/*   Updated: 2023/04/24 13:16:56 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/08/29 13:33:15 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int main(int argc, char **argv)
 		std::cout << "Error: need input data file for first argument\n";
 		return (1);
 	}
-	(void) argv;
 
 	BitcoinExchange bitcoin("./data.csv");
 	bitcoin.exchange(argv[1]);
